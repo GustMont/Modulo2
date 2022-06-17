bbda3236ca39a78105a35c87d7fa91e986f35e09
GustMont <gustavo.monteiro@sou.inteli.edu.br>
2022-06-17 12:05:44 -0300

    Exercicio 5 feito

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercicio 5/Exercicio5_Semana9.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercicio 5/Exercicio5_Semana9.js
@@ -6,10 +6,40 @@ function primeNumbers(){
     //puxando as informacoes do input do html colocando em variaveis locais da funcao
     var inicialNumberInJS = document.getElementById('inicialNumber').value
     var finalNumberInJS = document.getElementById('finalNumber').value
-    
-    console.log(inicialNumberInJS)
-    console.log(finalNumberInJS)
+
+    //array para armazenar resultados
+    var resultArray = []
+
+    //variavel utilizada para navegar pela array
+    var j = 0
+
+    //estrutura for para aplicar o valor máximo do intervalo
+    for(var i = 1; i <= finalNumberInJS; i++){
+        console.log("i "+i)
+
+        // estrutura if para verificar se o número da var i é primo
+        if(i == 2 || i == 3 || i == 5 || i == 7 || i == 11 || i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0 && i != 1){
+           
+            console.log("j "+j)
+
+            // se o numero for primo ele será armazenado na posicao da var j com o valor da var i 
+           if(i >= inicialNumberInJS){
+            resultArray[j] = i
+
+            // var j vai ser incrementada para avancar a posicao da array
+            j++
+            }
+        }
+    }
 
     
+    
+    console.log("inicialNumberInJS "+inicialNumberInJS)
+    console.log("finalNumberInJS "+finalNumberInJS)
+    console.log("resultArray "+resultArray)
+
+    //comando para permitir o html acessar o valor da variavel
+    document.getElementById("result").innerHTML=resultArray
+
 
 }
